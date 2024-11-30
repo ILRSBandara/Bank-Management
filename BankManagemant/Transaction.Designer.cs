892dd5168c64179093cc1f4b5fa6c305dbb9e644
Saundarya Iddamalgoda <160006775+ILRSBandara@users.noreply.github.com>
2024-11-30 22:10:19 +0530

    Add BTN Created

--- BankManagemant/Transaction.Designer.cs
+++ BankManagemant/Transaction.Designer.cs
@@ -138,6 +138,7 @@
             this.btnAdd.TabIndex = 11;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnSave
             // 
