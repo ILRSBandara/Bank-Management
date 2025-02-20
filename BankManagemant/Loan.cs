6a85fd757da2053efe5372fc518c771c26c57910
Saundarya Iddamalgoda <160006775+ILRSBandara@users.noreply.github.com>
2025-02-20 15:40:19 +0530

    Create Loan Save Btn

--- BankManagemant/Loan.cs
+++ BankManagemant/Loan.cs
@@ -45,12 +45,13 @@ namespace BankManagemant
         {
             SqlConnection con = new SqlConnection(@"Data Source=RASH\RASH;Initial Catalog=BankDB;Integrated Security=True;Encrypt=False");
             con.Open();
-            SqlCommand cnn = new SqlCommand("insert into loans values(@tid,@transaction_type,@amount,@transaction_date,@account_id)", con);
-            cnn.Parameters.AddWithValue("@TID", int.Parse(textBox1.Text));
-            cnn.Parameters.AddWithValue("@Transaction_Type", textBox2.Text);
+            SqlCommand cnn = new SqlCommand("insert into loans values(@loan_id,@loan_type,@amount,@interest_rate,@loan_date,@customer_name)", con);
+            cnn.Parameters.AddWithValue("@Loan_ID", int.Parse(textBox1.Text));
+            cnn.Parameters.AddWithValue("@Loan_Type", textBox2.Text);
             cnn.Parameters.AddWithValue("@Amount", int.Parse(textBox1.Text));
-            cnn.Parameters.AddWithValue("@Transaction_Date", dateTimePicker3.Value);
-            cnn.Parameters.AddWithValue("@Account_ID", int.Parse(textBox5.Text));
+            cnn.Parameters.AddWithValue("@Interest_Rate", textBox4.Text);
+            cnn.Parameters.AddWithValue("@Loan_Date", dateTimePicker3.Value);
+            cnn.Parameters.AddWithValue("@Customer_Name", textBox5.Text);
             cnn.ExecuteNonQuery();
             con.Close();
             MessageBox.Show("Record Saved Successfully!");
