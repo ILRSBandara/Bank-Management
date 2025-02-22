b4784dcf794927a4338818dc7d21d74fb6d6fe10
Saundarya Iddamalgoda <160006775+ILRSBandara@users.noreply.github.com>
2025-02-23 00:08:22 +0530

    Update Dashboard

--- BankManagemant/Dashboard.Designer.cs
+++ BankManagemant/Dashboard.Designer.cs
@@ -42,7 +42,6 @@
             this.panel1.BackColor = System.Drawing.Color.LightCyan;
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1553, 114);
@@ -60,6 +59,7 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cooper Black", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(399, 24);
